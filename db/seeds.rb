679c9429b3849ea03bd77d5ceb0b5a2e61b4d5dd
Andrew Patterson <andrew.patterson.3001@gmail.com>
2015-11-07 14:48:01 -0800

    add activity seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,21 @@ teacher = Teacher.create(
 
 (1..10).each do |i|
   Student.create(
-    name: "Student #{i}",
-    email: "student#{i}@school.edu",
+    name: "#{Faker::Name.first_name}",
+    email: "#{Faker::Name.last_name}@school.edu",
     teacher: teacher,
   )
 end
+
+10.times do EmotionCloudActivity.create(
+  cloud: {"surprise":0, "fear":1, "anger":1, "disgust":0, "sad":0, "happy":0})
+end
+
+10.times do JournalActivity.create(
+  content: "#{Faker::Lorem.paragraph}")
+end
+
+10.times do Assignment.create(
+  due: DateTime.now)
+end
+
