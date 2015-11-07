16412d035980c211acade3d448157e4b58b8fdd3
Scott Hardy <scott.the.hardy@gmail.com>
2015-11-07 15:31:47 -0800

    DOIN WORK

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
+  get 'teacher/dashboard'
+  get 'teacher/activities'
+  get 'teacher/students'
+  get 'teacher/students/:id', to: 'teacher#student', as: 'teacher_student'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'teacher#dashboard'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
